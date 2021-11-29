558d1a3f1341559c1d3954962baddebe3a02182f
philiprn <philiprutten1@gmail.com>
2021-11-29 12:46:55 +0100

    Update runsweep2.sh

--- runsweep2.sh
+++ runsweep2.sh
@@ -3,6 +3,6 @@ distances=($(seq 0.5 0.5 5))
 for i in "${filenumbers[@]}"
 do
         echo Running process $1 with distance ${distances[$i]}.
-        ./getcev2sweep $i ${distances[$i]}
+        Cpp/getcev2sweep $i ${distances[$i]}
         echo " "
 done
